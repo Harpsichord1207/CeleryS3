823e47586b27231f910e5c527dba39b1e58f37ed
Derek <liutao3@cig.com.cn>
2020-09-10 22:31:08 +0800

    works in windows

---
+++ README.md
@@ -0,0 +1,3 @@
+### PyCharm/Win 运行Celery Worker设置
+
+<img alt="CirclePacking" src="https://github.com/Harpsichord1207/CeleryS3/blob/master/images/1.png" width="300" height="300">
--- app.py
+++ app.py
@@ -1,22 +1,20 @@
-from __future__ import absolute_import
 import random
 import time
 
 from celery import Celery
 
-app = Celery(
-    "Test",
-    broker="redis://52.81.228.173:16379/",
-    backend="redis://52.81.228.173:16379/",
-    include=['app']
+celery_app = Celery(
+    "app",
+    broker="redis://52.81.228.173:16379/0",
+    backend="redis://52.81.228.173:16379/1",
 )
 
 
-@app.task
+@celery_app.task
 def add(x, y):
     time.sleep(random.randint(3, 6))
     return x + y
 
 
 if __name__ == '__main__':
-    app.start()
+    celery_app.start()
