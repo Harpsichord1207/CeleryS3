29d3363c401de7b9c36c1b8c3081aa703b25f482
Derek <liutao3@cig.com.cn>
2020-09-11 00:45:15 +0800

    works

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-### PyCharm/Win 运行Celery Worker设置
+### About
 
-<img alt="CirclePacking" src="https://github.com/Harpsichord1207/CeleryS3/blob/master/images/1.png" width="300" height="300">
+在复制许多s3文件时，可能要花很多时间，因此使用celery异步实现。  
+
+同时借鉴http://www.pythondoc.com/flask-celery/first.html,
+在html页面实现异步任务进度条。
+
+---
+
+### Run
+ - `celery worker -A app.celery_app --loglevel=debug -P threads`
+ - `python app.py`
--- app.py
+++ app.py
@@ -1,20 +1,74 @@
 import random
 import time
 
+from flask import Flask, render_template, jsonify, url_for, request
 from celery import Celery
 
+
+flask_app = Flask(__name__)
 celery_app = Celery(
-    "app",
+    flask_app.name,
     broker="redis://52.81.228.173:16379/0",
     backend="redis://52.81.228.173:16379/1",
 )
 
 
-@celery_app.task
-def add(x, y):
-    time.sleep(random.randint(3, 6))
-    return x + y
+@flask_app.route('/')
+def home():
+    return render_template('progress.html')
+
+
+@flask_app.route('/long_task', methods=['POST'])
+def long_task():
+    file_count = request.form.get('file_count', 100, type=int)
+    task = copy_s3_files.apply_async(args=[file_count])
+    return jsonify({}), 202, {'Location': url_for('task_status', task_id=task.id)}
+
+
+@flask_app.route('/status/<task_id>')
+def task_status(task_id):
+    task = copy_s3_files.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        response = {
+            'state': task.state,
+            'current': 0,
+            'total': 1,
+            'status': 'Pending...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'total': task.info.get('total', 1),
+            'status': task.info.get('status', '')
+        }
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    return jsonify(response)
+
+
+@celery_app.task(bind=True)
+def copy_s3_files(self, files_count):
+    # 模拟copy s3文件
+    for i in range(files_count):
+        time.sleep(random.randint(1, 5))
+        meta = {
+            'current': i + 1,
+            'total': files_count,
+            'status': 'finish file {}'.format(i+1)
+        }
+        self.update_state(state='PROGRESS', meta=meta)
+    # 这个是必须的吗？
+    return {'current': files_count, 'total': files_count, 'status': 'Task completed!', 'result': files_count}
 
 
 if __name__ == '__main__':
-    celery_app.start()
+    flask_app.run()
